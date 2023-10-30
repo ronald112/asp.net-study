e98f40e62f29cefdd95020527a164d9955aab71e
ronald1121 <ronald11211@gmail.com>
2023-10-30 23:28:46 +0000

    proj init

---
+++ Core/Program.cs
@@ -0,0 +1,26 @@
+using Core;
+
+var builder = WebApplication.CreateBuilder(args); // setting up a basic feature, login, configuuration etc..
+var app = builder.Build(); // set up middleware components
+
+//app.Use(async (context, next) =>
+//{
+//    if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "")
+//    {
+//        context.Response.ContentType = "text/plain";
+//        await context.Response.WriteAsync("Custom Middleware\n");
+//    }
+//    await next.Invoke();
+//    //await next(); // same as await next.Invoke();
+//});
+
+((IApplicationBuilder)app).Map("/branch", branch =>
+{
+    branch.Run(new Middleware().Invoke);
+});
+
+app.UseMiddleware<Middleware>();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
