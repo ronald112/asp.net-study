53bd833dc1c1aafe418bf28395dd182a24d20389
ronald1121 <ronald11211@gmail.com>
2023-11-09 16:02:55 +0000

    clean before interfaces

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,21 +1,6 @@
-using Core;
-
 var builder = WebApplication.CreateBuilder(args); // setting up a basic feature, login, configuuration etc..
 
-builder.Services.Configure<FruitOptions>(options =>
-{
-    options.Name = "Watermelon";
-    options.Color = "Green";
-});
-
 var app = builder.Build(); // set up middleware components
-
-((IApplicationBuilder)app).Map("/branch", branch =>
-{
-    branch.Run(new Middleware().Invoke);
-}); // /branch/?custom=true
-
-app.UseMiddleware<FruitMiddleware>(); // /fruit
 
 app.MapGet("/", () => "Hello World!");
 
